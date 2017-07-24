84cecdc295ebca72543d4f51e9dff81942ef17b5
Mohammed Anas Javeed <anasjaveed@users.noreply.github.com>
2017-07-24 19:01:15 +0530

    file which adds color to instabot.py

---
+++ colors.py
@@ -0,0 +1,14 @@
+#Colors for
+def prLightPurple(prt):print("\033[94m {}\033[00m".format(prt))
+def prGreen(prt): print("\033[92m {}\033[00m".format(prt))
+def prYellow(prt): print("\033[93m {}\033[00m".format(prt))
+def prPurple(prt): print("\033[95m {}\033[00m".format(prt))
+def prCyan(prt): print("\033[96m {}\033[00m".format(prt))
+def prRed(prt): print("\033[91m {}\033[00m" .format(prt))
+
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+O  = '\033[33m' # orange
+B  = '\033[34m' # blue
+P  = '\033[35m' # purple
+Black = '\033[1m' '\033[0m'
